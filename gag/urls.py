279084f482a12033f4fe8970dd927a903da6a4f1
sovietspy2 <kutya123>
2018-01-21 20:34:41 +0100

    fix

--- gag/urls.py
+++ gag/urls.py
@@ -1,9 +1,13 @@
 from . import views
 from django.urls import path
+from django.conf.urls import url
 
 app_name = 'gag'
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('posts/<int:pk>/', views.DetailsView.as_view(), name='details'),
+    #path('like/<int:id/', views.like, name='like'),
+    #url(r'like/(?P<int:id>\d+)/$', views.like, name='like')
+    path('like/<id>/', views.like, name='like')
 ]
--- gag/views.py
+++ gag/views.py
@@ -3,15 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.views import generic
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 
 def index(request):
     posts = Post.objects.all()
-    return render(request, 'index.html' ,{'posts': posts})
-    #return HttpResponse("hello world")
-
+    return render(request, 'gag/index.html', {'posts': posts})
+    #
 class DetailsView(generic.DetailView):
     model = Post
     context_object_name = 'post'
-    template_name = 'details.html'
+    template_name = 'gag/details.html'
+
+def like(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.likes = post.likes + 1
+    post.save()
+    posts = Post.objects.all()
+    return render(request, 'gag/index.html', {'posts': posts})
